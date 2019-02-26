cc88e5d97a181731eb508c4acb727c95b9b255a9
Josefine Flügge <jflygge@gmail.com>
2019-02-26 16:19:17 +0100

    la till ful kategoriväljare

--- main.js
+++ main.js
@@ -27,7 +27,7 @@ g.append("text")
  .attr("class", "caption")
  .attr("x", 0)
  .attr("y", -6)
- .text("Homocide");
+ .text("Freedom");
 var labels = ['0-1', '2-3', '4-5', '5-6', '7-8', '8-9', '10',];
 var legend = d3.legendColor()
  .labels(function (d) { return labels[d.i]; })
@@ -36,26 +36,65 @@ var legend = d3.legendColor()
 svg.select(".legendThreshold")
  .call(legend);
 
-// Load external data and boot
-d3.queue()
- .defer(d3.json, "http://enjalot.github.io/wwsd/data/world/world-110m.geojson")
- .defer(d3.csv, "hfi_cc_2018.csv", function(d) { data.set(d.ISO_code, +d.pf_ss_disappearances_disap); })
- .await(ready);
 
-function ready(error, topo) {
- if (error) throw error;
+//BUTTONS
+var buttonTitles = ["pf_ss_homicide", 'pf_ss_disappearances_disap'];
 
- // Draw the map
- svg.append("g")
-     .attr("class", "countries")
-     .selectAll("path")
-     .data(topo.features)
-     .enter().append("path")
-     .attr("fill", function (d){
-         // Pull data for this country
-         d.pf_ss_disappearances_disap = data.get(d.id) || 0;
-         // Set the color
-         return colorScale(d.pf_ss_disappearances_disap);
-     })
-     .attr("d", path);
+// Initialize the button
+var dropdownButton = d3.select("#UI")
+  .append('select')
+
+// add the options to the button
+dropdownButton // Add a button
+  .selectAll('myOptions') // Next 4 lines add 6 options = 6 colors
+ 	.data(buttonTitles)
+  .enter()
+	.append('option')
+  .text(function (d) { return d; }) // text showed in the menu
+  .attr("value", function (d) { return d; }) // corresponding value returned by the button
+
+
+
+// A function that update the color of the circle
+function updateMap(selectedOption) {
+
+        console.log("updating to :" + selectedOption);
+
+    
+            // Load external data and boot
+            d3.queue()
+            .defer(d3.json, "http://enjalot.github.io/wwsd/data/world/world-110m.geojson")
+            .defer(d3.csv, "hfi_cc_2018.csv", function(d) { 
+                data.set(d.ISO_code, +d[selectedOption]); })
+            .await(ready);
+
+            function ready(error, topo) {
+            if (error) throw error;
+
+                
+            // Draw the map
+            svg.append("g")
+                .attr("class", "countries")
+                .selectAll("path")
+                .data(topo.features)
+                .enter().append("path")
+                .attr("fill", function (d){
+                    // Pull data for this country
+                    d.selectedOption = data.get(d.id) || 0;
+                    // Set the color
+                    return colorScale(d.selectedOption);
+                })
+                .attr("d", path);
+            }
+
 }
+
+// When the button is changed, run the updateChart function
+dropdownButton.on("change", function(d) {
+
+    // recover the option that has been chosen
+    var selectedOption = d3.select(this).property("value")
+
+    // run the updateChart function with this selected option
+    updateMap(selectedOption)
+})
