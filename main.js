3f1e6f9e98fc0a7a7fd071852ef670e3517b262a
Josefine Flügge <jflygge@gmail.com>
2019-02-27 17:12:11 +0100

    added list of buttons from data and some layout

--- main.js
+++ main.js
@@ -37,64 +37,112 @@ svg.select(".legendThreshold")
  .call(legend);
 
 
-//BUTTONS
-var buttonTitles = ["pf_ss_homicide", 'pf_ss_disappearances_disap'];
+//BUTTONS LIST
+var buttonTitles = [];
+// Initialize button
+var listButton = d3.select("#UI2")
 
-// Initialize the button
-var dropdownButton = d3.select("#UI")
-  .append('select')
-
-// add the options to the button
-dropdownButton // Add a button
-  .selectAll('myOptions') // Next 4 lines add 6 options = 6 colors
- 	.data(buttonTitles)
-  .enter()
-	.append('option')
-  .text(function (d) { return d; }) // text showed in the menu
-  .attr("value", function (d) { return d; }) // corresponding value returned by the button
+//On application start
+InitalLoadAndDraw("pf_rol_procedural");
 
 
+//Draw map from data on start, create buttontitles
+function InitalLoadAndDraw(selectedOption){
 
-// A function that update the color of the circle
-function updateMap(selectedOption) {
+    var gotLines = false;
 
-        console.log("updating to :" + selectedOption);
+     // Load external data and boot
+     d3.queue()
+     .defer(d3.json, "http://enjalot.github.io/wwsd/data/world/world-110m.geojson")
+     .defer(d3.csv, "hfi_cc_2018.csv", function(d) { 
+        data.set(d.ISO_code, +d[selectedOption]); 
+        
+        //get the headers aka categories to add to buttons in gui, only once
+        if(!gotLines){   
+            var dataHeaders = d3.keys(d);
+        
+            //store header titles related to index in buttonTitles
+            dataHeaders.forEach(function(item, index) {
+                if(index >= 4){
+                    buttonTitles.push(item);
+                }
+            });
 
-    
-            // Load external data and boot
-            d3.queue()
-            .defer(d3.json, "http://enjalot.github.io/wwsd/data/world/world-110m.geojson")
-            .defer(d3.csv, "hfi_cc_2018.csv", function(d) { 
-                data.set(d.ISO_code, +d[selectedOption]); })
-            .await(ready);
+            console.log("buttonTitles:", buttonTitles);
 
-            function ready(error, topo) {
-            if (error) throw error;
+            // add the options to the button
+            listButton
+            .selectAll("myOptions")
+            .data(buttonTitles).enter()
+            .append("option")
+            .attr("type","button")
+            .attr("class","button")
+            .text(function (d) { return d; }) // text showed in the menu
+            .attr("value", function (d) {return d;})
+            .on("click", function() {  // recover the option that has been chosen
+              
+                var selectedOption = d3.select(this).property("value")
 
-                
-            // Draw the map
-            svg.append("g")
-                .attr("class", "countries")
-                .selectAll("path")
-                .data(topo.features)
-                .enter().append("path")
-                .attr("fill", function (d){
-                    // Pull data for this country
-                    d.selectedOption = data.get(d.id) || 0;
-                    // Set the color
-                    return colorScale(d.selectedOption);
-                })
-                .attr("d", path);
-            }
+                console.log("updating to :" + selectedOption);
+         
+                // When the button is changed, redraw the map according to selected option
+                LoadAndDraw(selectedOption)
+            })       
+
+            gotLines = true;
+        }
+    }).await(ready);
+
+     
+     function ready(error, topo){
+     if (error) throw error;
+
+     // Draw the map
+     svg.append("g")
+         .attr("class", "countries")
+         .selectAll("path")
+         .data(topo.features)
+         .enter().append("path")
+         .attr("fill", function (d){
+             // Pull data for this country
+             d.selectedOption = data.get(d.id) || 0;
+             // Set the color
+             return colorScale(d.selectedOption);
+         })
+         .attr("d", path);
+     }
+     
+}
+
+//Load data and draw map
+function LoadAndDraw(selectedOption){
+
+    // Load external data and boot
+    d3.queue()
+    .defer(d3.json, "http://enjalot.github.io/wwsd/data/world/world-110m.geojson")
+    .defer(d3.csv, "hfi_cc_2018.csv", function(d) { 
+            data.set(d.ISO_code, +d[selectedOption]); })
+    .await(ready);
+
+    function ready(error, topo) {
+        if (error) throw error;
+   
+           //console.log(data.columns);
+   
+        // Draw the map
+        svg.append("g")
+            .attr("class", "countries")
+            .selectAll("path")
+            .data(topo.features)
+            .enter().append("path")
+            .attr("fill", function (d){
+                // Pull data for this country
+                d.selectedOption = data.get(d.id) || 0;
+                // Set the color
+                return colorScale(d.selectedOption);
+            })
+            .attr("d", path);
+        }
 
 }
 
-// When the button is changed, run the updateChart function
-dropdownButton.on("change", function(d) {
-
-    // recover the option that has been chosen
-    var selectedOption = d3.select(this).property("value")
-
-    // run the updateChart function with this selected option
-    updateMap(selectedOption)
-})
