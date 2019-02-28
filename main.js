69f9fc75f18eb2c317929c20205769882fb1881e
Aniisa Bihi <aniisaaden@gmail.com>
2019-02-28 11:19:42 +0100

    Revert "Merge remote-tracking branch 'origin/master'"
    This reverts commit 1fa6354482a7fafaa104c15c8f0b1da54989a6e9, reversing
    changes made to 3c673edfbbe942317a61d5eb08885ccc5f4a2d66.

--- main.js
+++ main.js
@@ -16,6 +16,7 @@ dropdownButton // Add a button
   .text(function (d) { return d; }) // text showed in the menu
   .attr("value", function (d) { return d; }) // corresponding value returned by the button
 
+
 // A function that update the color of the circle
 function updateMap(selectedOption) {
         console.log("updating to :" + selectedOption);    
@@ -29,6 +30,7 @@ function updateMap(selectedOption) {
             function ready(error, topo) {
             if (error) throw error;
 
+                
             // Draw the map
             svg.append("g")
                 .attr("class", "countries")
@@ -51,167 +53,15 @@ function updateMap(selectedOption) {
                 })
                 .attr("d", path);
             }
+
 }
 
 // When the button is changed, run the updateChart function
 dropdownButton.on("change", function(d) {
+
     // recover the option that has been chosen
     var selectedOption = d3.select(this).property("value")
 
     // run the updateChart function with this selected option
     updateMap(selectedOption)
 })
-
- this.tooltip = function(d) {
-     //Helper function for including information tool_tip
-     // Defining tooltip for hovering points
-     var tooltip = d3.select("#tooltip")
-     tooltip
-         .select("#countries")
-         .text("Country: " + d.properties.name)
-
-     tooltip
-         .select("#value")
-         .text("Value: " + d.selectedOption)
- }
-
-// HOVER FUNCTION
- $("path").hover(function(e) {
-     $('#myTooltip').tooltip();
-     $('#info-box').css('display','block');
-     $('#info-box').html($(this).data('info'));
- });
-
- $("path").mouseleave(function(e) {
-     $('#info-box').css('display','none');
- });
-
- $(document).mousemove(function(e) {
-     $('#info-box').css('top',e.pageY-$('#info-box').height()-30);
-     $('#info-box').css('left',e.pageX-($('#info-box').width())/2);
- }).mouseover();
-
-//BUTTONS LIST
-var buttonTitles = [];
-// Initialize button
-var listButton = d3.select("#UI2")
-
-//On application start
-InitalLoadAndDraw("pf_rol_procedural");
-
-
-//Draw map from data on start, create buttontitles
-function InitalLoadAndDraw(selectedOption){
-
-    var gotLines = false;
-
-     // Load external data and boot
-     d3.queue()
-     .defer(d3.json, "http://enjalot.github.io/wwsd/data/world/world-110m.geojson")
-     .defer(d3.csv, "hfi_cc_2018.csv", function(d) { 
-        data.set(d.ISO_code, +d[selectedOption]); 
-        
-        //get the headers aka categories to add to buttons in gui, only once
-        if(!gotLines){   
-            var dataHeaders = d3.keys(d);
-        
-            //store header titles related to index in buttonTitles
-            dataHeaders.forEach(function(item, index) {
-                if(index >= 4){
-                    buttonTitles.push(item);
-                }
-            });
-
-            console.log("buttonTitles:", buttonTitles);
-
-            // add the options to the button
-            listButton
-            .selectAll("myOptions")
-            .data(buttonTitles).enter()
-            .append("option")
-            .attr("type","button")
-            .attr("class","button")
-            .text(function (d) { return d; }) // text showed in the menu
-            .attr("value", function (d) {return d;})
-            .on("click", function() {  // recover the option that has been chosen
-              
-                var selectedOption = d3.select(this).property("value")
-
-                console.log("updating to :" + selectedOption);
-         
-                // When the button is changed, redraw the map according to selected option
-                LoadAndDraw(selectedOption)
-            })       
-
-            gotLines = true;
-        }
-    }).await(ready);
-
-     
-    function ready(error, topo) {
-        if (error) throw error;
-
-            
-        // Draw the map
-        svg.append("g")
-            .attr("class", "countries")
-            .selectAll("path")
-            .data(topo.features)
-            .enter().append("path")
-            .attr("fill", function (d){
-                // Pull data for this country
-                d.selectedOption = data.get(d.id) || 0;
-                // Set the color
-                return colorScale(d.selectedOption);
-            })
-            .style("fill", function (d){
-                // Pull data for this country
-                d.selectedOption = data.get(d.id) || 0;
-                // Set the color
-                if(d.selectedOption == 10) return "url(#fireGradient)"
-                else if(d.selectedOption == 0) return "url(#circles-2)";
-        
-            })
-            .attr("d", path);
-        }
-    }     
-
-
-//Load data and draw map
-function LoadAndDraw(selectedOption){
-
-    // Load external data and boot
-    d3.queue()
-    .defer(d3.json, "http://enjalot.github.io/wwsd/data/world/world-110m.geojson")
-    .defer(d3.csv, "hfi_cc_2018.csv", function(d) { 
-            data.set(d.ISO_code, +d[selectedOption]); })
-    .await(ready);
-
-    function ready(error, topo) {
-        if (error) throw error;
-
-            
-        // Draw the map
-        svg.append("g")
-            .attr("class", "countries")
-            .selectAll("path")
-            .data(topo.features)
-            .enter().append("path")
-            .attr("fill", function (d){
-                // Pull data for this country
-                d.selectedOption = data.get(d.id) || 0;
-                // Set the color
-                return colorScale(d.selectedOption);
-            })
-            .style("fill", function (d){
-                // Pull data for this country
-                d.selectedOption = data.get(d.id) || 0;
-                // Set the color
-                if(d.selectedOption == 10) return "url(#fireGradient)"
-                else if(d.selectedOption == 0) return "url(#circles-2)";
-        
-            })
-            .attr("d", path);
-        }
-
-}
